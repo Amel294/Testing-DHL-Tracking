3998657d9296488a050f8a21e974e6207594834f
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-05 14:05:23 +0530

    test 2

--- server.js
+++ server.js
@@ -12,6 +12,44 @@ app.post('/webhook', (req, res) => {
   res.status(200).send('Received');
 });
 
+const axios = require('axios');
+let data = JSON.stringify({
+  "format": "Default",
+  "service": "express",
+  "hook": {
+    "uri": "https://webhook123.mydomain.com/track/push",
+    "authentication": {
+      "type": "ApiKeyAuthentication",
+      "header": "pa$$word",
+      "token": "pa$$word"
+    }
+  },
+  "events": [
+    "delivered"
+  ],
+  "type": "Account",
+  "accountID": "string"
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api-test.dhl.com/tracking/push/v1/subscription',
+  headers: { 
+    'DHL-API-Key': 'process.env.DHL_API_KEY',
+  'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
