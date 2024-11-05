fa5225fb78f6541b565b8b288044afdf6187b91e
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-05 13:58:19 +0530

    hello world

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
-
+app.get('/',(req,res)=>{
+  res.send('Hello World');
+})
 app.post('/webhook', (req, res) => {
   console.log('Received webhook:', req.body);
   res.status(200).send('Received');
